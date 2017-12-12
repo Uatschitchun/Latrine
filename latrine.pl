db7334b8360743e89c496add4f97af0dec32874d
Uatschitchun <Anregend@yahoo.de>
2017-12-12 09:42:14 +0100

    Update latrine.pl

--- latrine.pl
+++ latrine.pl
@@ -29,17 +29,18 @@ my $speed = param('speed') || '';
 my $acc = param('acc') || '';
 # Bearing - current GPS bearing (in degree)
 my $bearing = param('bearing') || '';
-# Time - current GPS time (in format defined by user. You may choose from predefined styles or define you own by this specification)
+# Time - current GPS time (in format defined by user. You may choose from predefined styles or define you own by this specifica
+ion)
 # my $time = param('time') || '';
 # Text field - text field with own define key/value pair.
 # my $var = param('var') || '';
 
 # Check storage location
 if ($store =~ /\/$/) {
-	unless (-d $store) { mkdir($store, 0770) }
-	unless (-d $store) { die( "Can\'t create $store: $!" ) }
+        unless (-d $store) { mkdir($store, 0770) }
+        unless (-d $store) { die( "Can\'t create $store: $!" ) }
 } else {
-	die 'You forgot the trailing slash to indicate a directory!';
+        die 'You forgot the trailing slash to indicate a directory!';
 }
 
 # Current Unix timestamp
@@ -51,37 +52,38 @@ print header('application/json');
 print '{';
 # Check private key
 if ($key =~ /^[\d\w]{5,15}$/ ) {
-	$store .= "$key.latlon"; # Create storage location
-	# Save latitude, longitude and other data into text file
-	if ( $lat =~ /^(\-?\d+(\.\d+))$/ && $lon =~ /^(\-?\d+(\.\d+))$/ ) {
-		$alt = '0' unless ($alt > 0 && $alt <= 10000);
-		$speed = '0' unless ($speed > 0 && $speed <= 900);
-		$bearing = '0' unless ($bearing > 0 && $bearing <= 360);
-		$acc = '0' unless ($acc > 0 && $acc <= 1000);
-		if (open STORE, "> $store") {
-			print STORE "$lat:$lon:$alt:$speed:$bearing:$acc:$time";
-			close STORE;
-		} else {
-			print '"error":"Can\'t open filestore"';
-		}
-	# Output latitude, longitude and other data as JSON
-	} elsif ($view == '1' ) {
-		if (open STORE, "< $store") {
-			while(<STORE>) {
-				($lat,$lon,$alt,$speed,$bearing,$acc,$time) = split(/:/,$_) if $_ =~ /^(\-?\d+(\.\d+))/;
-			}
-			print qq ~ "lat":"$lat","lon":"$lon","alt":"$alt","speed":"$speed","bearing":"$bearing","acc":"$acc","time":"$time","now":"$now"~;
-			close STORE;
-		} else {
-			print '"error":"Can\'t open filestore"';
-		}
-	# Parameters are missing
-	} else {
-		print '"error":"Please provide at least lat and lon parameters"';
-	}
+        $store .= "$key.latlon"; # Create storage location
+        # Save latitude, longitude and other data into text file
+        if ( $lat =~ /^(\-?\d+(\.\d+))$/ && $lon =~ /^(\-?\d+(\.\d+))$/ ) {
+                $alt = '0' unless ($alt > 0 && $alt <= 10000);
+                $speed = '0' unless ($speed > 0 && $speed <= 900);
+                $bearing = '0' unless ($bearing > 0 && $bearing <= 360);
+                $acc = '0' unless ($acc > 0 && $acc <= 1000);
+                if (open STORE, "> $store") {
+                        print STORE "$lat:$lon:$alt:$speed:$bearing:$acc:$time";
+                        close STORE;
+                } else {
+                        print '"error":"Can\'t open filestore"';
+                }
+        # Output latitude, longitude and other data as JSON
+        } elsif ($view == '1' ) {
+                if (open STORE, "< $store") {
+                        while(<STORE>) {
+                                ($lat,$lon,$alt,$speed,$bearing,$acc,$time) = split(/:/,$_) if $_ =~ /^(\-?\d+(\.\d+))/;
+                        }
+                        print qq ~ "lat":"$lat","lon":"$lon","alt":"$alt","speed":"$speed","bearing":"$bearing","acc":"$acc","t
+me":"$time","now":"$now"~;
+                        close STORE;
+                } else {
+                        print '"error":"Can\'t open filestore"';
+                }
+        # Parameters are missing
+        } else {
+                print '"error":"Please provide at least lat and lon parameters"';
+        }
 # Private key is missing
 } else {
-	print '"error":"Please provide your private key (at least 5 characters up to 15 characters a-zA-Z0-9_)."';
+        print '"error":"Please provide your private key (at least 5 characters up to 15 characters a-zA-Z0-9_)."';
 }
 # Done!
 print '}';
